24df15d1388a4bcba2e18265dea6d451512ed96a
Bill <biliff@fsu.edu>
2014-12-13 21:37:33 -0500

    Finish Section 1.2

--- 1_2/1_4.c
+++ 1_2/1_4.c
@@ -5,21 +5,34 @@
    floating-point version
  */
 
+// Exercise 1-4
+
 int main()
 {
   float fahr, celsius;
   int upper, lower, step;
 
+  char* fahrs = "Fahrenheit";
+  char* cels = "Celsius";
+
+  FILE *fp;
+
   lower = 0;   // lower limit of temperature table
   upper = 300; // upper limit of temperature table
   step = 20;   // step size
-
   fahr = lower;
+
+  fp = fopen("output.txt","w");
+
+  fprintf(fp, "%3s\t %6s\n", fahrs, cels);
+  
   while(fahr <= upper){
     celsius = (5.0/9.0) * (fahr - 32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
+    fprintf(fp, "%5.0f\t\t %6.1f\n", fahr, celsius);
 	   fahr = fahr + step;
   }
+
+  fclose(fp);
 
   return 1;
 
